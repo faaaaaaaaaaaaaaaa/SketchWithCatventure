b25029b7874bbf845831724c86021eed26b9932c
Kanpitcha Sinitwarakun <43286850+faaaaaaaaaaaaaaaa@users.noreply.github.com>
2020-04-19 01:41:02 +0700

    2-1

---
+++ Assets/Scripts/stat/stat.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class stat 
+{
+    [SerializeField]
+    private int basevalue;
+
+    public int GetValue ()
+    {
+        return basevalue;
+    }
+}
